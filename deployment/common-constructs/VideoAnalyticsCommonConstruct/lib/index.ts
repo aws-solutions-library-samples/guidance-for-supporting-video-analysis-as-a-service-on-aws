a29d5925387481020c72029d19dd846453a05dd3
Zizhao Zhang <zzizhao@amazon.com>
2024-10-15 13:49:53 -0700

    add in build script for DeviceManagementCDK and refactor consumption model for Common construct

--- deployment/common-constructs/VideoAnalyticsCommonConstruct/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { VideoAnalyticsAsyncWorkflowResource, Workflow, WorkflowProps, GsiProps, WorkflowLambdaProps } from './video_analytics_async_workflow';
-export { createLambdaRole, createBasicLambdaRole } from './serviceConstructs/iamUtils';
-export { createTable, createGSI } from './serviceConstructs/ddbUtils';
