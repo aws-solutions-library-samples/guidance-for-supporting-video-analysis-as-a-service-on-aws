69d746f538e294ab2ac2ee47236eaf253eb040e9
zangshim <zangshim@amazon.com>
2024-10-29 11:10:23 -0700

    Add video logistics bootstrap stack
    Add video logistics bootstrap stack

--- deployment/device-management-cdk/VideoAnalyticsDeviceManagementCDK/lib/stacks/workflowStacks/startDeleteDeviceNotification.ts
+++ deployment/device-management-cdk/VideoAnalyticsDeviceManagementCDK/lib/stacks/workflowStacks/startDeleteDeviceNotification.ts
@@ -1,17 +1,24 @@
-import type { Role } from 'aws-cdk-lib/aws-iam';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Fail, JsonPath, StateMachine, Succeed, TaskInput } from 'aws-cdk-lib/aws-stepfunctions';
+import type { Role } from "aws-cdk-lib/aws-iam";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import {
+  Fail,
+  JsonPath,
+  StateMachine,
+  Succeed,
+  TaskInput,
+} from "aws-cdk-lib/aws-stepfunctions";
 import {
   VideoAnalyticsAsyncWorkflowResource,
-  createLambdaRole, AWSRegionUtils,
-  AWSRegion
-} from 'video_analytics_common_construct/';
-import { Function, Code } from 'aws-cdk-lib/aws-lambda';
-import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
-import { LambdaInvoke } from 'aws-cdk-lib/aws-stepfunctions-tasks';
-import { Duration } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { WorkflowStackProps } from './workflowStack';
+  createLambdaRole,
+  AWSRegionUtils,
+  AWSRegion,
+} from "video_analytics_common_construct/";
+import { Function, Code } from "aws-cdk-lib/aws-lambda";
+import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
+import { LambdaInvoke } from "aws-cdk-lib/aws-stepfunctions-tasks";
+import { Duration } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { WorkflowStackProps } from "./workflowStack";
 import {
   ERROR_MESSAGE_PATH,
   LAMBDA_MEMORY_SIZE_KB,
@@ -21,15 +28,15 @@ import {
   RESULT_PATH,
   RESULT_PATH_ERROR,
   LAMBDA_ASSET_PATH_TO_DEVICE_MANAGEMENT,
-  DM_WORKFLOW_JAVA_PATH_PREFIX
-} from '../const';
+  DM_WORKFLOW_JAVA_PATH_PREFIX,
+} from "../const";
 
 /**
  *
  */
 export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowResource {
-  partitionKeyName = 'JobId';
-  name = 'DeleteDeviceNotificationTable';
+  partitionKeyName = "JobId";
+  name = "DeleteDeviceNotificationTable";
 
   private readonly dynamoDbStatement: PolicyStatement;
   private readonly kmsStatement: PolicyStatement;
@@ -44,33 +51,35 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
     this.dynamoDbStatement = new PolicyStatement({
       effect: Effect.ALLOW,
       actions: [
-        'dynamodb:GetRecords',
-        'dynamodb:GetItem',
-        'dynamodb:Query',
-        'dynamodb:PutItem',
-        'dynamodb:UpdateItem'
+        "dynamodb:GetRecords",
+        "dynamodb:GetItem",
+        "dynamodb:Query",
+        "dynamodb:PutItem",
+        "dynamodb:UpdateItem",
       ],
-      resources: ['*'] // This will be updated in postWorkflowCreationCallback
+      resources: ["*"], // This will be updated in postWorkflowCreationCallback
     });
 
     this.kmsStatement = new PolicyStatement({
       effect: Effect.ALLOW,
-      actions: ['kms:Decrypt', 'kms:Encrypt', 'kms:ReEncrypt*'],
-      resources: ['*'] // This will be updated in postWorkflowCreationCallback
+      actions: ["kms:Decrypt", "kms:Encrypt", "kms:ReEncrypt*"],
+      resources: ["*"], // This will be updated in postWorkflowCreationCallback
     });
 
-    this.role = createLambdaRole(this, 'StartDeleteDeviceNotificationRole', [
+    this.role = createLambdaRole(this, "StartDeleteDeviceNotificationRole", [
       this.dynamoDbStatement,
       this.kmsStatement,
     ]);
     this.region = props.region;
-    this.airportCode = AWSRegionUtils.getAirportCode(props.region).toLowerCase();
+    this.airportCode = AWSRegionUtils.getAirportCode(
+      props.region
+    ).toLowerCase();
     this.account = props.account;
   }
 
   createStepFunction(): void {
-    const failState = new Fail(this, 'Fail');
-    const successState = new Succeed(this, 'Successful');
+    const failState = new Fail(this, "Fail");
+    const successState = new Succeed(this, "Successful");
 
     /*
      * Define Lambdas for Step function here.
@@ -79,15 +88,14 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
      */
     const detachNotificationPermissionFromDeviceCertLambda = new Function(
       this,
-      'DetachNotificationPermissionFromDeviceCertLambda',
+      "DetachNotificationPermissionFromDeviceCertLambda",
       {
         //TODO: Update this if any changes are made to the lambda handler path or asset built jar location
         code: Code.fromAsset(`${LAMBDA_ASSET_PATH_TO_DEVICE_MANAGEMENT}`),
         description:
-          'Lambda responsible for detaching notification permission from the device cert',
+          "Lambda responsible for detaching notification permission from the device cert",
         runtime: LAMBDA_RUNTIME,
-        handler:
-          `${DM_WORKFLOW_JAVA_PATH_PREFIX}deletedevicenotif.DetachNotificationPermissionFromDeviceCertHandler::handleRequest`,
+        handler: `${DM_WORKFLOW_JAVA_PATH_PREFIX}deletedevicenotif.DetachNotificationPermissionFromDeviceCertHandler::handleRequest`,
         memorySize: LAMBDA_MEMORY_SIZE_KB,
         role: this.role,
         environment: {
@@ -95,23 +103,28 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
           LambdaRoleArn: this.role.roleArn,
         },
         timeout: LAMBDA_TIMEOUT_MINUTES,
-        logGroup: new LogGroup(this, 'DetachNotificationPermissionFromDeviceCertLambdaLogGroup', {
-          retention: RetentionDays.TEN_YEARS,
-          logGroupName: 'DetachNotificationPermissionFromDeviceCertLambdaLogGroup'
-        })
+        logGroup: new LogGroup(
+          this,
+          "DetachNotificationPermissionFromDeviceCertLambdaLogGroup",
+          {
+            retention: RetentionDays.TEN_YEARS,
+            logGroupName:
+              "DetachNotificationPermissionFromDeviceCertLambdaLogGroup",
+          }
+        ),
       }
     );
 
     const removeNotificationTopicFromShadowLambda = new Function(
       this,
-      'RemoveNotificationTopicFromShadowLambda',
+      "RemoveNotificationTopicFromShadowLambda",
       {
         //TODO: Update this if any changes are made to the lambda handler path or asset built jar location
         code: Code.fromAsset(`${LAMBDA_ASSET_PATH_TO_DEVICE_MANAGEMENT}`),
-        description: 'Lambda responsible for removing notification topic from the shadow',
+        description:
+          "Lambda responsible for removing notification topic from the shadow",
         runtime: LAMBDA_RUNTIME,
-        handler:
-          `${DM_WORKFLOW_JAVA_PATH_PREFIX}deletedevicenotif.RemoveNotificationTopicFromShadowHandler::handleRequest`,
+        handler: `${DM_WORKFLOW_JAVA_PATH_PREFIX}deletedevicenotif.RemoveNotificationTopicFromShadowHandler::handleRequest`,
         memorySize: LAMBDA_MEMORY_SIZE_KB,
         role: this.role,
         environment: {
@@ -119,24 +132,27 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
           LambdaRoleArn: this.role.roleArn,
         },
         timeout: LAMBDA_TIMEOUT_MINUTES,
-        logGroup: new LogGroup(this, 'RemoveNotificationTopicFromShadowLambdaLogGroup', {
-          retention: RetentionDays.TEN_YEARS,
-          logGroupName: 'RemoveNotificationTopicFromShadowLambdaLogGroup'
-        })
+        logGroup: new LogGroup(
+          this,
+          "RemoveNotificationTopicFromShadowLambdaLogGroup",
+          {
+            retention: RetentionDays.TEN_YEARS,
+            logGroupName: "RemoveNotificationTopicFromShadowLambdaLogGroup",
+          }
+        ),
       }
     );
 
     const updateDeleteDeviceNotificationStatusStateLambda = new Function(
       this,
-      'UpdateDeleteDeviceNotificationStatusLambda',
+      "UpdateDeleteDeviceNotificationStatusLambda",
       {
         //TODO: Update this if any changes are made to the lambda handler path or asset built jar location
         code: Code.fromAsset(`${LAMBDA_ASSET_PATH_TO_DEVICE_MANAGEMENT}`),
         description:
-          'Lambda responsible for updating the state of the Delete Device Notification Job',
+          "Lambda responsible for updating the state of the Delete Device Notification Job",
         runtime: LAMBDA_RUNTIME,
-        handler:
-          `${DM_WORKFLOW_JAVA_PATH_PREFIX}deletedevicenotif.UpdateDeleteDeviceNotificationStatusStateHandler::handleRequest`,
+        handler: `${DM_WORKFLOW_JAVA_PATH_PREFIX}deletedevicenotif.UpdateDeleteDeviceNotificationStatusStateHandler::handleRequest`,
         memorySize: LAMBDA_MEMORY_SIZE_KB,
         role: this.role,
         environment: {
@@ -144,23 +160,26 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
           LambdaRoleArn: this.role.roleArn,
         },
         timeout: LAMBDA_TIMEOUT_MINUTES,
-        logGroup: new LogGroup(this, 'UpdateDeleteDeviceNotificationStatusLambdaLogGroup', {
-          retention: RetentionDays.TEN_YEARS,
-          logGroupName: 'UpdateDeleteDeviceNotificationStatusLambdaLogGroup'
-        })
+        logGroup: new LogGroup(
+          this,
+          "UpdateDeleteDeviceNotificationStatusLambdaLogGroup",
+          {
+            retention: RetentionDays.TEN_YEARS,
+            logGroupName: "UpdateDeleteDeviceNotificationStatusLambdaLogGroup",
+          }
+        ),
       }
     );
 
     const failDeleteDeviceNotificationStatusStateLambda = new Function(
       this,
-      'FailDeleteDeviceNotificationStatusLambda',
+      "FailDeleteDeviceNotificationStatusLambda",
       {
         //TODO: Update this if any changes are made to the lambda handler path or asset built jar location
         code: Code.fromAsset(`${LAMBDA_ASSET_PATH_TO_DEVICE_MANAGEMENT}`),
-        description: 'Lambda responsible for marking as failed',
+        description: "Lambda responsible for marking as failed",
         runtime: LAMBDA_RUNTIME,
-        handler:
-          `${DM_WORKFLOW_JAVA_PATH_PREFIX}deletedevicenotif.FailDeleteDeviceNotificationStatusStateHandler::handleRequest`,
+        handler: `${DM_WORKFLOW_JAVA_PATH_PREFIX}deletedevicenotif.FailDeleteDeviceNotificationStatusStateHandler::handleRequest`,
         memorySize: LAMBDA_MEMORY_SIZE_KB,
         role: this.role,
         environment: {
@@ -168,10 +187,14 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
           LambdaRoleArn: this.role.roleArn,
         },
         timeout: LAMBDA_TIMEOUT_MINUTES,
-        logGroup: new LogGroup(this, 'FailDeleteDeviceNotificationStatusLambdaLogGroup', {
-          retention: RetentionDays.TEN_YEARS,
-          logGroupName: 'FailDeleteDeviceNotificationStatusLambdaLogGroup'
-        })
+        logGroup: new LogGroup(
+          this,
+          "FailDeleteDeviceNotificationStatusLambdaLogGroup",
+          {
+            retention: RetentionDays.TEN_YEARS,
+            logGroupName: "FailDeleteDeviceNotificationStatusLambdaLogGroup",
+          }
+        ),
       }
     );
 
@@ -180,50 +203,50 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
      */
     const detachNotificationPermissionFromDeviceCertState = new LambdaInvoke(
       this,
-      'DetachNotificationPermissionFromDeviceCert',
+      "DetachNotificationPermissionFromDeviceCert",
       {
         lambdaFunction: detachNotificationPermissionFromDeviceCertLambda,
         payload: TaskInput.fromObject({
-          jobId: JsonPath.stringAt(PARTITION_KEY_PATH)
+          jobId: JsonPath.stringAt(PARTITION_KEY_PATH),
         }),
-        resultPath: RESULT_PATH
+        resultPath: RESULT_PATH,
       }
     );
 
     const removeNotificationTopicFromShadowState = new LambdaInvoke(
       this,
-      'RemoveNotificationTopicFromShadow',
+      "RemoveNotificationTopicFromShadow",
       {
         lambdaFunction: removeNotificationTopicFromShadowLambda,
         payload: TaskInput.fromObject({
-          jobId: JsonPath.stringAt(PARTITION_KEY_PATH)
+          jobId: JsonPath.stringAt(PARTITION_KEY_PATH),
         }),
-        resultPath: RESULT_PATH
+        resultPath: RESULT_PATH,
       }
     );
 
     const updateDeleteDeviceNotificationStatusState = new LambdaInvoke(
       this,
-      'UpdateDeleteDeviceNotificationStatus',
+      "UpdateDeleteDeviceNotificationStatus",
       {
         lambdaFunction: updateDeleteDeviceNotificationStatusStateLambda,
         payload: TaskInput.fromObject({
-          jobId: JsonPath.stringAt(PARTITION_KEY_PATH)
+          jobId: JsonPath.stringAt(PARTITION_KEY_PATH),
         }),
-        resultPath: RESULT_PATH
+        resultPath: RESULT_PATH,
       }
     );
 
     const failDeleteDeviceNotificationStatusState = new LambdaInvoke(
       this,
-      'FailDeleteDeviceNotificationStatus',
+      "FailDeleteDeviceNotificationStatus",
       {
         lambdaFunction: failDeleteDeviceNotificationStatusStateLambda,
         payload: TaskInput.fromObject({
           jobId: JsonPath.stringAt(PARTITION_KEY_PATH),
-          failureReason: JsonPath.stringAt(ERROR_MESSAGE_PATH)
+          failureReason: JsonPath.stringAt(ERROR_MESSAGE_PATH),
         }),
-        resultPath: RESULT_PATH
+        resultPath: RESULT_PATH,
       }
     );
 
@@ -232,47 +255,63 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
      */
 
     // Step 1 : Detach notification permision from device cert
-    detachNotificationPermissionFromDeviceCertState.next(removeNotificationTopicFromShadowState);
+    detachNotificationPermissionFromDeviceCertState.next(
+      removeNotificationTopicFromShadowState
+    );
     detachNotificationPermissionFromDeviceCertState.addRetry({
       //TODO: Update this once the model code is updated
-      errors: ['com.amazon.awsvideoanalyticsdmcontrolplane.exceptions.RetryableException'],
+      errors: [
+        "com.amazon.awsvideoanalyticsdmcontrolplane.exceptions.RetryableException",
+      ],
       interval: Duration.seconds(2),
       maxAttempts: 5,
-      backoffRate: 2
+      backoffRate: 2,
     });
     detachNotificationPermissionFromDeviceCertState.addCatch(
       failDeleteDeviceNotificationStatusState,
       {
-        resultPath: RESULT_PATH_ERROR
+        resultPath: RESULT_PATH_ERROR,
       }
     );
 
     // Step 2 : Remove notification topic from shadow
-    removeNotificationTopicFromShadowState.next(updateDeleteDeviceNotificationStatusState);
+    removeNotificationTopicFromShadowState.next(
+      updateDeleteDeviceNotificationStatusState
+    );
     removeNotificationTopicFromShadowState.addRetry({
       //TODO: Update this once the model code is updated
-      errors: ['com.amazon.awsvideoanalyticsdmcontrolplane.exceptions.RetryableException'],
+      errors: [
+        "com.amazon.awsvideoanalyticsdmcontrolplane.exceptions.RetryableException",
+      ],
       interval: Duration.seconds(2),
       maxAttempts: 5,
-      backoffRate: 2
+      backoffRate: 2,
     });
-    removeNotificationTopicFromShadowState.addCatch(failDeleteDeviceNotificationStatusState, {
-      resultPath: RESULT_PATH_ERROR
-    });
+    removeNotificationTopicFromShadowState.addCatch(
+      failDeleteDeviceNotificationStatusState,
+      {
+        resultPath: RESULT_PATH_ERROR,
+      }
+    );
 
     // Step 3 : Update the Notification Rule Creation Job state to successful.
     // End of workflow if it is successful.
     updateDeleteDeviceNotificationStatusState.next(successState);
     updateDeleteDeviceNotificationStatusState.addRetry({
       //TODO: Update this once the model code is updated
-      errors: ['com.amazon.awsvideoanalyticsdmcontrolplane.exceptions.RetryableException'],
+      errors: [
+        "com.amazon.awsvideoanalyticsdmcontrolplane.exceptions.RetryableException",
+      ],
       interval: Duration.seconds(2),
       maxAttempts: 5,
-      backoffRate: 2
+      backoffRate: 2,
     });
-    updateDeleteDeviceNotificationStatusState.addCatch(failDeleteDeviceNotificationStatusState, {
-      resultPath: RESULT_PATH_ERROR
-    });
+    updateDeleteDeviceNotificationStatusState.addCatch(
+      failDeleteDeviceNotificationStatusState,
+      {
+        resultPath: RESULT_PATH_ERROR,
+      }
+    );
 
     // Failure step, End of workflow if it fails
     failDeleteDeviceNotificationStatusState.next(failState);
@@ -283,9 +322,9 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
      */
     const deleteNotificationRuleStateMachine = new StateMachine(
       this,
-      'StartDeleteDeviceNotificationStateMachine',
+      "StartDeleteDeviceNotificationStateMachine",
       {
-        definition: detachNotificationPermissionFromDeviceCertState
+        definition: detachNotificationPermissionFromDeviceCertState,
       }
     );
     this.stateMachine = deleteNotificationRuleStateMachine;
@@ -300,7 +339,7 @@ export class StartDeleteDeviceNotification extends VideoAnalyticsAsyncWorkflowRe
         this.kmsStatement.addResources(encryptionKey.keyArn);
       }
     } else {
-      throw new Error('Workflow or workflow table is not defined');
+      throw new Error("Workflow or workflow table is not defined");
     }
   }
 
--- deployment/device-management-cdk/VideoAnalyticsDeviceManagementCDK/test/workflowStack.test.ts
+++ deployment/device-management-cdk/VideoAnalyticsDeviceManagementCDK/test/workflowStack.test.ts
@@ -1,20 +1,20 @@
-import { describe, test, expect } from '@jest/globals';
-import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import { WorkflowStack } from '../lib/stacks/workflowStacks/workflowStack';
-import { AWSRegion } from 'video_analytics_common_construct';
+import { describe, test, expect } from "@jest/globals";
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { WorkflowStack } from "../lib/stacks/workflowStacks/workflowStack";
+import { AWSRegion } from "video_analytics_common_construct";
 
-describe('WorkflowStack', () => {
-  test('Snapshot test', () => {
+describe("WorkflowStack", () => {
+  test("Snapshot test", () => {
     const app = new cdk.App();
-    const stack = new WorkflowStack(app, 'MyTestStack', {
+    const stack = new WorkflowStack(app, "MyTestStack", {
       env: {
-        account: '123456789012',
-        region: 'us-east-1',
+        account: "123456789012",
+        region: "us-east-1",
       },
       resources: [],
-      region: 'us-east-1' as AWSRegion,
-      account: '123456789012',
+      region: "us-east-1" as AWSRegion,
+      account: "123456789012",
     });
 
     const template = Template.fromStack(stack);
