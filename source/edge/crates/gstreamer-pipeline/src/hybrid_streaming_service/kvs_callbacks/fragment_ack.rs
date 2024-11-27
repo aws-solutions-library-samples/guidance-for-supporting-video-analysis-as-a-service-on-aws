d35d30e9832e87aa9b860cf97f1e46eaaca9f7d6
Manit Singh Kalsi <manitk@amazon.com>
2024-11-27 14:16:47 -0800

    removing OfflineKVSFrameAckManager

--- source/edge/crates/gstreamer-pipeline/src/hybrid_streaming_service/kvs_callbacks/fragment_ack.rs
+++ source/edge/crates/gstreamer-pipeline/src/hybrid_streaming_service/kvs_callbacks/fragment_ack.rs
@@ -2,7 +2,6 @@
 use crossbeam_channel::{unbounded, Receiver, Sender, TrySendError};
 use glib::once_cell::sync::Lazy;
 use std::sync::{Arc, Mutex};
-pub(crate) type OfflineTimeStampMs = u64;
 
 /// The Fragment ack manager is held in static memory so callbacks can access channels to notify realtime + offline client
 /// that fragments have persisted in KVS cloud.  Used for realtime callbacks.
@@ -12,21 +11,6 @@ pub(super) static KVS_FRAGMENT_REALTIME_ACK_MANAGER: Lazy<Arc<Mutex<RealtimeKVSF
         let manager = RealtimeKVSFrameAckManager { sender, receiver };
         Arc::new(Mutex::new(manager))
     });
-/// The Fragment ack manager is held in static memory so callbacks can access channels to notify realtime + offline client
-/// that fragments have persisted in KVS cloud.  Used for offline callbacks.
-pub(super) static KVS_FRAGMENT_OFFLINE_ACK_MANAGER: Lazy<Arc<Mutex<OfflineKVSFrameAckManager>>> =
-    Lazy::new(|| {
-        let (sender, receiver) = unbounded::<OfflineTimeStampMs>();
-        let manager = OfflineKVSFrameAckManager { sender, receiver };
-        Arc::new(Mutex::new(manager))
-    });
-
-/// Blocking but gets the global kvs fragment ack receiver channel.
-pub(crate) fn get_kvs_fragment_rx_channel_for_offline() -> Receiver<OfflineTimeStampMs> {
-    let mut fragment_ack_lock =
-        KVS_FRAGMENT_OFFLINE_ACK_MANAGER.lock().expect("KVS Fragment Ack Mutex is poisoned.");
-    fragment_ack_lock.get_callback_receiver()
-}
 
 /// Blocking but gets the global kvs fragment ack receiver channel.
 pub(crate) fn get_kvs_fragment_rx_channel_for_realtime() -> Receiver<KVSReceiver> {
@@ -48,25 +32,6 @@ impl KVSReceiver {
             KVSReceiver::FrameTimeCode(code) => code,
             _ => panic!("Expected FrameTimeCode variant"),
         }
-    }
-}
-
-/// Struct to hold frame data in memory till frame acknowledgement is received from KVS.
-pub(crate) struct OfflineKVSFrameAckManager {
-    sender: Sender<u64>,
-    receiver: Receiver<u64>,
-}
-
-impl OfflineKVSFrameAckManager {
-    pub(crate) fn get_callback_receiver(&mut self) -> Receiver<u64> {
-        self.receiver.clone()
-    }
-
-    pub(crate) fn try_send_persisted_fragment_time_code(
-        &mut self,
-        persisted_fragment_time_code: u64,
-    ) -> Result<(), TrySendError<u64>> {
-        self.sender.try_send(persisted_fragment_time_code)
     }
 }
 
